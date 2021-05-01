c87b0df3aae43ae2ca7df26296486904217280e5
JERRY-NOTE <77578839+JERRY-NOTE@users.noreply.github.com>
2021-05-01 15:50:08 +0800

    Add files via upload

---
+++ ColorFinder/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  ColorFinder
+//
+//  Created by user on 2021/4/30.
+//  Copyright © 2021 JerryStdudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    var redValue = 255
+    var greenValue = 255
+    var blueValue = 255
+    
+    
+    @IBOutlet weak var redText: UILabel!
+    
+    @IBOutlet weak var greenText: UILabel!
+    
+    @IBOutlet weak var blueText: UILabel!
+    
+    
+    
+    
+    @IBAction func redSliderChanged(_ sender: UISlider) {
+        redValue = Int(sender.value)
+        redText.text = "R:\(redValue)"
+        changeBackgroundColor()
+    }
+    
+    @IBAction func greenSliderChanged(_ sender: UISlider) {
+        greenValue = Int(sender.value)
+        greenText.text = "G:\(greenValue)"
+        changeBackgroundColor()
+    }
+    
+    @IBAction func blueSliderChanged(_ sender: UISlider) {
+        blueValue = Int(sender.value)
+        blueText.text = "B:\(blueValue)"
+        changeBackgroundColor()
+    }
+    
+    func changeBackgroundColor(){
+        view.backgroundColor = UIColor(red: CGFloat(redValue) / 255, green: CGFloat(greenValue) / 255, blue: CGFloat(blueValue) / 255, alpha: 1)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
